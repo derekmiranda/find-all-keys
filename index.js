9d9ec5b6fda4f2dcfa16c9f030c1f5913eb4398d
Derek Miranda <derek.miranda@ff0000.com>
2018-05-22 17:58:08 -0700

    findAllKeys util built w/ basic tests

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * Similar to Lodash Object find function, except returns all instances
+ * where predicate either equals recursively found values
+ * or predicate evaluates to a truthy value when current value is passed in.
+ * 
+ * returns Array of matching keys
+ * obj: Object
+ * predicate: Any (match by value) / Function (value matches if predicate evals to truthy)
+ */
+
+// cache to prevent revisiting seen keys
+const _seenObjs = new Map()
+
+function findAllKeys(obj, predicate) {
+	return _searchObj(obj, predicate)
+}
+
+function _searchObj(obj, predicate, baseKey = '') {
+	// mark obj as seen
+	_seenObjs.set(obj, true)
+	
+	const keys = Object.keys(obj)
+	let matchingKeys = []
+	
+	for (const key of keys) {
+		const val = obj[key]
+		
+		if (typeof val === 'object' && _seenObjs.has(val)) {
+			continue
+		}
+		
+		const keyStr = baseKey ? `${baseKey}.${key}` : key
+		if (typeof predicate === 'function' && predicate(val)) {
+			matchingKeys.push(keyStr)
+		} else if (val === predicate) {
+			matchingKeys.push(keyStr)
+		} else if (typeof val === 'object') {
+			const recursivelyFoundKeys = _searchObj(val, predicate, keyStr)
+
+			if (recursivelyFoundKeys) {
+				matchingKeys = matchingKeys.concat(recursivelyFoundKeys)
+			}
+		}
+	}
+	
+	return matchingKeys.length ? matchingKeys : undefined
+}
+
+module.exports = findAllKeys
---
+++ test.js
@@ -0,0 +1,53 @@
+import test from 'ava'
+import findAllKeys from './index'
+
+test('Returns matching keys of top-level values', t => {
+	const obj = {
+		a: 1,
+		b: 2,
+		c: 1
+	}
+	t.deepEqual(findAllKeys(obj, 1), ['a', 'c'])
+})
+
+test("Returns undefined if can't find val", t => {
+	const obj = {
+		a: 1,
+		b: 2,
+		c: 1
+	}
+	t.is(findAllKeys(obj, 3), undefined)
+})
+
+test("Matches keys by predicate function", t => {
+	const obj = {
+		a: 1,
+		b: 2,
+		c: -1
+	}
+	const pred = n => n < 0
+	t.deepEqual(findAllKeys(obj, pred), ['c'])
+})
+
+test("Looks recursively thru objects w/ predicate function", t => {
+	const obj = {
+		a: 1,
+		b: 2,
+		c: {
+			d: 3,
+			e: {
+				f: 5
+			}
+		}
+	}
+	const isOdd = n => n % 2
+	t.deepEqual(findAllKeys(obj, isOdd), ['a', 'c.d', 'c.e.f'])
+})
+
+test("Prevents looping if finds circular reference", t => {
+	const a = { c: 3 }
+	const b = { a }
+	a.b = b
+
+	t.deepEqual(findAllKeys(a, 3), ['c'])
+})
